699f7208335e8bd8d7cbd40a756d3a5e782cd099
ahnguyen.vn <ahnnguyen.vn@colorverse.co>
2023-10-19 17:29:02 +0700

    feat: scene improvement by CuongHTran

--- Assets/Scripts/StringsContainer.cs
+++ Assets/Scripts/StringsContainer.cs
@@ -11,7 +11,7 @@ public class StringsContainer : MonoBehaviour
     private void Awake()
     {
         // since amino acid order starts from 1, we ignore the first element in the list.
-        atomCharacters.Add(new AtomCharacter());
+        atomCharacters?.Add(new AtomCharacter());
     }
 
     private void OnEnable()
