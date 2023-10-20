de5be141fa5503b3e5f04c29e2f114775dd446d5
Cuong Tran <cuongtran.vn@colorverse.co>
2023-10-20 15:49:10 +0700

    feat: add atom detector

---
+++ Assets/Scripts/AtomDetector.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AtomDetector : MonoBehaviour
+{
+    [SerializeField] private float _maxRayDistance = 1.0f;
+
+    private List<HighlightedMeshItem> _meshesInRange = new List<HighlightedMeshItem>();
+    private HighlightedMeshItem _nearestItem;
+    private bool _itemInRange = false;
+    private RaycastHit _raycastHit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<HighlightedMeshItem>(out var meshItem))
+        {
+            if (!_meshesInRange.Contains(meshItem))
+            {
+                _itemInRange = true;
+                _meshesInRange.Add(meshItem);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<HighlightedMeshItem>(out var meshItem))
+        {
+            if (_meshesInRange.Contains(meshItem))
+            {
+                _meshesInRange.Remove(meshItem);
+            }
+            if (_meshesInRange.Count == 0)
+            {
+                _nearestItem = null;
+                _itemInRange = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (!_itemInRange)
+            return;
+
+
+        if (Physics.Raycast(transform.position, transform.forward, out _raycastHit, _maxRayDistance))
+        {
+            if (_raycastHit.transform.TryGetComponent<HighlightedMeshItem>(out var meshItem))
+            {
+                _nearestItem = meshItem;
+            }
+        }
+        else
+        {
+            _nearestItem = null;
+        }
+
+        if (_nearestItem != null)
+            HighlightedMeshItem.SetHighlighted(_nearestItem._info.Order, true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward);
+    }
+}
